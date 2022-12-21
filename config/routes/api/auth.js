d41061aa1f83c41518b7a3e70914cc8448b29fbe
Kae <kaegandah14@gmail.com>
2022-12-21 13:51:29 +0800

    User Auth / LogIn Route

--- config/routes/api/auth.js
+++ config/routes/api/auth.js
@@ -1,6 +1,12 @@
 const express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../../middleware/auth');
+const gravatar = require('gravatar');
+
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { body, validationResult } = require('express-validator');
 
 const User = require('../../../models/User');
 
@@ -11,6 +17,78 @@ router.get('/', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route       POST api/auth
+// @desc        Authenticate user & get token
+// @access      Public
+router.post('/', 
+[
+    body('email', 'Please include a valid email').isEmail(),
+    body(
+        'password', 
+        'Please is required').exists()
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try{
+    // See if user exists
+        let user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400)
+            .json({ 
+                errors: [{
+                    msg: 'Invalid Credentials'}] 
+                });
+        }
+
+    // Get users gravator
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+    // Match email and password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400)
+            .json({ 
+                errors: [{
+                    msg: 'Invalid Credentials'}] 
+                });
+        }
+
+       
+     // Return jsonwebtoken
+      const payload = {
+        user: {
+            id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+            {expiresIn: 360000},
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+        );
+
     } catch(err){
         console.error(err.message);
         res.status(500).send('Server error');
