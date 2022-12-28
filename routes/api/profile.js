df62eef3d06f670d420c902253a8ea6cb5b5dbf2
Ariane Kae <kaegandah14@gmail.com>
2022-12-28 18:07:39 +0800

    Delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -118,7 +118,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-// @route       GET api/profile/user/uer_id
+// @route       GET api/profile/user/:user_id
 // @desc        Get profile by user ID
 // @access      Public
 
@@ -141,5 +141,24 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send('Server error');
     }
 });
+
+// @route       DELETE api/profile
+// @desc        Delete profile, user & posts
+// @access      Private
+router.delete('/', auth, async (req, res) => {
+    try{
+        // @todo -- remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User deleted' });
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
